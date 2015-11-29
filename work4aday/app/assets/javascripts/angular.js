bfc1c8426a751021f807a03b76d2af17286e83cf
Yoan Ante <yoanante@gmail.com>
2015-11-29 00:05:20 -0500

    Have the jobposting page posting to the database but not updating on the view yet.

--- work4aday/app/assets/javascripts/angular.js
+++ work4aday/app/assets/javascripts/angular.js
@@ -5,29 +5,33 @@ var app = angular.module('work4aday', []);
 ///////////////////Header Controller//////////////////////////
   app.controller("HeaderController",['$http', function($http){
     var controller = this;
+    //Get current_user from route
     $http.get('/session').success(function(data){
+      //currrent_user comes back nested in current_user so we use data.current_user to unwrap
       controller.current_user = data.current_user
       console.log(controller);
     });
   }]);
 
 
-
+////////////////////Post Controller/////////////////////////
   app.controller("PostController", function ($http) {
-    var thisController = this;
+    var controller = this;
+
+    // get authenticity_token from the DOM (rails injects it on load)
     csrf = $('meta[name=csrf-token]').attr('content')
 
+    // create a job posting
     this.createJobposting = function () {
       $http({
         url: '/jobpostings',
         method: 'POST',
         data: {
-          jobposting: thisController.newJobposting,
+          jobposting: controller.newJobposting,
           authenticity_token: csrf
         }
       }).then(function (success){
-        thisController.getJobpostings();
-        // console.log(success);
+        console.log(success);
       }, function (error){
         console.log(error);
       });
@@ -35,12 +39,12 @@ var app = angular.module('work4aday', []);
 
 
     this.getJobposting = function () {
-      console.log("I'm here");
       $http({
         url: '/jobpostings',
         method: 'GET'
       }).then(function (success){
-        console.log(success);
+        controller.jobposting = success.data.jobpostings
+        console.log(success.data.jobpostings);
       }, function (error){
         console.log(error);
       });
